bc01104fcba48fe033026e657640f8ce2a957472
Helgi Hrafn Gunnarsson <helgi@binary.is>
2020-10-14 20:04:07 +0000

    Basic local_settings.py arrangement started for customizable settings.

--- poetrybox/settings.py
+++ poetrybox/settings.py
@@ -15,18 +15,8 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@gm-nrg64r=((gns751nx8kr*3awkpa-z^3lg3@2n6_bu25zi)'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -118,3 +108,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Import customizable settings.
+from poetrybox.local_settings import *
