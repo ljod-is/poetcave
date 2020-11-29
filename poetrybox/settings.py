b22b0c91f30ad85197430237deb67c54ebcc0545
Helgi Hrafn Gunnarsson <helgi@binary.is>
2020-11-29 16:55:15 +0000

    Translation mechanism implemented.

--- poetrybox/settings.py
+++ poetrybox/settings.py
@@ -28,12 +28,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'languagecontrol',
+
     'core',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'languagecontrol.middleware.LanguageControlMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -95,15 +99,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
-
+LANGUAGE_CODE = 'is'
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
+LOCALE_PATHS = [
+    BASE_DIR / 'poetrybox' / 'locale',
+]
 
 
 # Static files (CSS, JavaScript, Images)
