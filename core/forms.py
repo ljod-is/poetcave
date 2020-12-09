7b4ff4a0e63ff8e166dd5cdf808779884be2ada8
Helgi Hrafn Gunnarsson <helgi@binary.is>
2020-12-09 18:31:31 +0000

    Missing registration fields added to view, template and form.

--- core/forms.py
+++ core/forms.py
@@ -4,3 +4,18 @@ from django_registration.forms import RegistrationForm as BaseRegistrationForm
 class RegistrationForm(BaseRegistrationForm):
     class Meta(BaseRegistrationForm.Meta):
         model = User
+        fields = [
+            'username',
+            'email',
+            'password1',
+            'password2',
+            'name',
+            'name_dative',
+            'birth_year',
+            'about',
+            'contact_name',
+            'contact_address',
+            'contact_postal_code',
+            'contact_place',
+            'contact_phone',
+        ]
--- core/models.py
+++ core/models.py
@@ -2,16 +2,16 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    name = models.CharField(max_length=100)
-    name_dative = models.CharField(max_length=100)
-    birth_year = models.SmallIntegerField(null=True)
+    name = models.CharField(max_length=100, null=False, blank=False)
+    name_dative = models.CharField(max_length=100, null=False, blank=False)
+    birth_year = models.SmallIntegerField(null=True, blank=True)
 
-    about = models.TextField()
+    about = models.TextField(null=True, blank=True)
 
     # Fields beginning with `contact_` are only intende for use by
     # administrators, not for displaying on the web.
     contact_name = models.CharField(max_length=100)
-    contact_address = models.CharField(max_length=100)
-    contact_postal_code = models.CharField(max_length=3)
-    contact_place = models.CharField(max_length=100)
-    contact_phone = models.CharField(max_length=30)
+    contact_address = models.CharField(max_length=100, null=True, blank=True)
+    contact_postal_code = models.CharField(max_length=3, null=True, blank=True)
+    contact_place = models.CharField(max_length=100, null=True, blank=True)
+    contact_phone = models.CharField(max_length=30, null=True, blank=True)
