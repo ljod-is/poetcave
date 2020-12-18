1ad7396e53cad9bc251a4bf4fbaea053b3afade9
Helgi Hrafn Gunnarsson <helgi@binary.is>
2020-12-18 20:15:47 +0000

    Password can be updated through profile. (Custom password-change view also included but disabled.)

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import include
 from django.urls import path
 from django_registration.backends.activation.views import RegistrationView
@@ -10,4 +11,15 @@ urlpatterns = [
     path('accounts/profile/', views.profile, name='profile'),
     path('accounts/', include('django_registration.backends.activation.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+
+    # We're not using this for now, since passwords are changed through the
+    # profile, but this code remains in case we'll want to change the flow.
+    #path(
+    #    'accounts/password_change/',
+    #    auth_views.PasswordChangeView.as_view(
+    #        template_name='registration/change_password_form.html',
+    #        success_url = '/'
+    #    ),
+    #    name='change_password'
+    #),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,12 @@
 from core.forms import ProfileForm
+from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib import messages
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.urls import reverse
+from django.utils.translation import ugettext as _
 
 def main(request):
     return render(request, 'core/main.html')
@@ -11,17 +15,46 @@ def main(request):
 @login_required
 def profile(request):
 
+    password_form = PasswordChangeForm(request.user)
+    form = ProfileForm(instance=request.user)
+
     if request.method == 'POST':
         form = ProfileForm(request.POST, instance=request.user)
 
         if form.is_valid():
-            form.save()
-            return redirect(reverse('profile'))
 
-    else:
-        form = ProfileForm(instance=request.user)
+            # Check for password change.
+            # If password change fails, we don't want to redirect back to the
+            # profile with success, but rather for the password form to
+            # express the errors to users and allow them to try again.
+            password_ok = False
+            if request.POST.get('old_password') or request.POST.get('new_password1') or request.POST.get('new_password2'):
+                password_form = PasswordChangeForm(request.user, request.POST)
+                if password_form.is_valid():
+                    password_form.save()
+                    update_session_auth_hash(request, password_form.user)
+
+                    messages.add_message(request, messages.SUCCESS, _('Password updated.'))
+
+                    # Okay because changing passwords succeeded.
+                    password_ok = True
+            else:
+                # Okay because changing passwords wasn't even attempted.
+                password_ok = True
+
+            # Even if password change didn't succeed, we still believe in the
+            # main form data at this point, so we might as well save it.
+            if form.has_changed():
+                form.save()
+                messages.add_message(request, messages.SUCCESS, _('Profile updated.'))
+
+            # Let's not redirect if we want the password form to express
+            # errors to the user.
+            if password_ok:
+                return redirect(reverse('profile'))
 
     ctx = {
+        'password_form': password_form,
         'form': form,
     }
     return render(request, 'core/profile.html', ctx)
