07f6cb8b525de39d49cfcaa66d14f9df9eccf31c
Helgi Hrafn Gunnarsson <helgi@binary.is>
2021-05-15 18:14:45 +0000

    Duplicate emails during registration not allowed anymore. Old duplicates retained.

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,22 @@
 from core.models import User
 from django import forms
+from django.utils.translation import ugettext as _
 from django_registration.forms import RegistrationForm as BaseRegistrationForm
 
+# A custom email form field which checks if the email address is already
+# taken. This is done instead of simply having the email-field as unique,
+# because we are working with data that already has duplicates and we want to
+# use moving forward. So we want to retain email duplicates from the past, but
+# reject them in new registrations.
+class UniqueEmailField(forms.EmailField):
+    def validate(self, value):
+        super().validate(value)
+        if User.objects.filter(email=value).count() > 0:
+            raise forms.ValidationError(_('Email address is already in use.'))
+
 class RegistrationForm(BaseRegistrationForm):
+    email = UniqueEmailField()
+
     class Meta(BaseRegistrationForm.Meta):
         model = User
         fields = [
